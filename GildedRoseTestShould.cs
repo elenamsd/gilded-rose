bbf74e3895b6afd4a1ef0df0d7d03d465247c16d
Elena Sacramento <elena.sacramento@leanmind.es>
2024-01-30 10:37:21 +0000

    DecreaseQualityAndSellInOfRegularItem

---
+++ GildedRoseTestShould.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseTestShould
+    {
+        [Test]
+        public void DecreaseQualityAndSellInOfRegularItem()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "IrrelevantItem", SellIn = 5, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(4, Items[0].SellIn);
+            Assert.AreEqual(4, Items[0].Quality);
+        }
+    }
+}
